08fca2fd05ceb545ac1dca4f509471b916671a3b
gidwikintan <gidwikintan@gmail.com>
2022-07-10 14:20:15 +0700

    feat(setup): :mag: create documeny file for SEO and setup font

---
+++ pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+        <Head>
+          <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@200;300;400;500;700;800&display=swap" rel="stylesheet" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
