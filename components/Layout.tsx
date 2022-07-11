5dc9b0d7fef3c9625b0d5b5549f528bf640d3126
gidwikintan <gidwikintan@gmail.com>
2022-07-11 19:49:54 +0700

    feat(layuot): :sparkles:layouting search input

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Sidebar from './Sidebar'
+import Search from './Search'
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
       <div>
           <Sidebar />
           <main className='flex flex-col py-6 px-4 mx-0 md:ml-6 md:pt-0  lg:ml-32 lg:min-w-[800px] lg:grow lg:mt-9'>
-              {children}
+          <Search />      
+        {children}
           </main>
     </div>
   )
---
+++ components/Search/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {BiSearch} from 'react-icons/bi'
+
+function Search() {
+  return (
+      <div className='flex bg-transparent text-white mb-5 items-center text-xl'>
+          <BiSearch size={30} className="mr-2" />
+          <input type="text" placeholder='Find your favooo!' className='bg-transparent focus:border-0 w-full px-2'/>
+    </div>
+  )
+}
+
+export default Search
