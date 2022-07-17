e04dec7730b5a76a844849429f2f1b1fc2ede41a
giwangdk <gidwikintan@gmail.com>
2022-07-17 21:28:28 +0700

    feat(setup): :sparkles: setup api endpoints for fetching data

--- utils/imd.ts
+++ utils/imd.ts
@@ -1,2 +1,10 @@
+import { getWithoutHeader } from './api';
+
 const API_URL = process.env.IMDB_API_URL;
 const API_KEY = process.env.IMDB_API_KEY;
+
+export const getTop250Movies = getWithoutHeader(`${API_URL}/Top250Movies/${API_KEY}`);
+
+export const getTop250TVs = getWithoutHeader(`${API_URL}/Top250TVs/${API_KEY}`);
+
+export const getComingSoon = getWithoutHeader(`${API_URL}/ComingSoon/${API_KEY}`);
