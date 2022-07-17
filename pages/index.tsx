7494332888e4cd1352f08b2d940a93602af5c5c2
giwangdk <gidwikintan@gmail.com>
2022-07-17 22:23:32 +0700

    feat(fetching): :sparkles: using getstaic props to get data :construction:

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
+import { GetStaticProps } from 'next'
 import CardMovie from '../components/CardMovie'
 import Layout from '../components/Layout'
 import Trending from '../components/Trending'
+import { getComingSoon } from '../utils/imd'
 
-function IndexPage() {
+function IndexPage({ comingSoon }) {
+  console.log(comingSoon)
   return (
     <Layout>
       <Trending />
@@ -11,4 +14,14 @@ function IndexPage() {
   )
 }
 
+export const getStaticProps:GetStaticProps = async () => {
+  const res = await getComingSoon();
+  const comingSoon = res.data.data;
+  console.log(res)
+  return {
+    props: {
+      comingSoon
+    },
+  }
+}
 export default IndexPage
--- utils/api.ts
+++ utils/api.ts
@@ -1,37 +1,11 @@
 /* eslint-disable no-alert */
 import axios from 'axios'
 
-export const handleNetworkError = (error) => {
-  if (error.message === 'Network request failed') {
-    alert(
-      'Kesalahan Jaringan',
-      'Silakan periksa koneksi Anda dan coba kembali.',
-    );
-  }
-  throw error;
-};
-
-export const handleCommonError = (error) => {
-  if (error && error.data.msg === 'invalid token') {
-    alert(
-      'Session kamu telah habis',
-      'Silakan login kembali dengan akun kamu yg telah terdaftar'
-    );
-    throw new Error({
-      logout: true
-    });
-  } else {
-    throw error;
-  }
-};
-
-export const getWithoutHeader = (api:string) => (timeout = true) => {
+export const getWithoutHeader = (api:string) => {
   return axios(
     api,
     {
       method: 'GET'
     },
-    { handleNetworkError, handleCommonError },
-    timeout
   );
 };
