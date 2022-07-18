edcea692ec3d0acde4173db21f61dff2e57ed119
giwangdk <gidwikintan@gmail.com>
2022-07-18 23:04:43 +0700

    feat(styling): :feat: improving image thumbnail style

--- components/CardThumbnail/index.tsx
+++ components/CardThumbnail/index.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image'
-import MovieImg from '../../assets/medium.jpg'
 import { Movie } from '../../interfaces'
 import MovieDesc from '../CardMovie/MovieDesc'
 
@@ -7,7 +6,7 @@ function CardThumbnail({ movie }: {movie: Movie}) {
   return (
     <div className=" relative ">
       <div className="rounded-xl">
-        <Image src={movie.image} className="rounded-xl object-fit" width="600" height="330" />
+        <Image src={movie.image} className="rounded-xl object-cover object-bottom" width="600" height="330" />
       </div>
       <div className="desc absolute z-40 bottom-5 left-5 text-white">
         <MovieDesc movie={movie} />
