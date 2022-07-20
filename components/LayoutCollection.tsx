d2ba74773f0227b1c385d91805bdb9d67998d732
giwangdk <gidwikintan@gmail.com>
2022-07-20 17:38:36 +0700

    feat(layout): :art: improving styles for heading in layout collection and implement it

--- components/LayoutCollection.tsx
+++ components/LayoutCollection.tsx
@@ -2,10 +2,14 @@ import React from 'react'
 
 function LayoutCollection({ children, title }: { children: React.ReactNode, title: string }) {
   return (
-    <section className="">
-      <h2>{title}</h2>
-      {children}
-    </section>
+
+    <>
+      <h2 className="text-5xl text-white font-bold">{title}</h2>
+      <section className="py-6">
+        {children}
+      </section>
+
+    </>
   )
 }
 
--- components/Trending/index.tsx
+++ components/Trending/index.tsx
@@ -5,25 +5,28 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import CardThumbnail from '../CardThumbnail';
 import { Movie } from '../../interfaces';
+import LayoutCollection from '../LayoutCollection';
 
 function Trending({ movies }: { movies: Movie[] }) {
   return (
-    <div className="w-full">
+    <LayoutCollection title="Trending">
+      <div className="w-full">
 
-      <Swiper
-        slidesPerView={3}
-        spaceBetween={30}
-        scrollbar={{ draggable: true }}
-        onSlideChange={() => console.log('slide change')}
-        onSwiper={(swiper) => console.log(swiper)}
-      >
-        {
-          (movies || []).slice(0, 5).map((movie) => (
-            <SwiperSlide key={movie.id}><CardThumbnail movie={movie} /></SwiperSlide>
-          ))
-        }
-      </Swiper>
-    </div>
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={30}
+          scrollbar={{ draggable: true }}
+          onSlideChange={() => console.log('slide change')}
+          onSwiper={(swiper) => console.log(swiper)}
+        >
+          {
+    (movies || []).slice(0, 5).map((movie) => (
+      <SwiperSlide key={movie.id}><CardThumbnail movie={movie} /></SwiperSlide>
+    ))
+  }
+        </Swiper>
+      </div>
+    </LayoutCollection>
   )
 }
 
