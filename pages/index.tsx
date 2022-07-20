4c5caf2bf85c1a50c94bd8e1560c803596033a7d
giwangdk <gidwikintan@gmail.com>
2022-07-20 17:55:47 +0700

    feat(integrate): :sparkles: init API for fetching data movies

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,21 +8,23 @@ import { getWithoutHeader } from '../utils/api'
 const API_URL = process.env.IMDB_API_URL;
 const API_KEY = process.env.IMDB_API_KEY;
 
-function IndexPage({ comingSoon }: { comingSoon: Movie[] }) {
+function IndexPage({ comingSoon, movies }: { comingSoon: Movie[], movies: Movie[] }) {
   return (
     <Layout>
       <Trending movies={comingSoon} />
-      <CardMovie />
+      <CardMovie movies={movies} />
     </Layout>
   )
 }
 
 export const getStaticProps:GetStaticProps = async () => {
-  const res = await getWithoutHeader(`${API_URL}/Top250TVs/${API_KEY}`);
+  const res = await getWithoutHeader(`${API_URL}/ComingSoon/${API_KEY}`); const resMovie = await getWithoutHeader(`${API_URL}/Top250Movies/${API_KEY}`);
   const comingSoon:Movie[] = await res.data.items;
+  const movies:Movie[] = await resMovie.data.items;
   return {
     props: {
-      comingSoon
+      comingSoon,
+      movies
     }
   }
 }
